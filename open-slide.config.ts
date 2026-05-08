6e1d68183c671fc75be483553bf14425f313c334
Yuan Chi <yuan.chi@program.com.tw>
2026-05-08 11:02:39 +0800

    chore: init open-slide project

---
+++ open-slide.config.ts
@@ -0,0 +1,8 @@
+import type { OpenSlideConfig } from '@open-slide/core';
+import { zhTW } from '@open-slide/core/locale';
+
+const openSlideConfig: OpenSlideConfig = {
+  locale: zhTW,
+};
+
+export default openSlideConfig;
